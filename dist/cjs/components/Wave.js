2c03f53fc9dda2e5427b1947a08471c957924fd1
Maxim Buz <mbuz.maxim@gmail.com>
2022-04-26 17:59:27 +0200

    new build

--- dist/cjs/components/Wave.js
+++ dist/cjs/components/Wave.js
@@ -7,17 +7,17 @@ const react_1 = __importDefault(require("react"));
 const useMeasure_1 = __importDefault(require("../utils/calculations/useMeasure"));
 const stacked_1 = require("../utils/calculations/stacked");
 const Wave = (props) => {
-    const [ref, { width, height }] = (0, useMeasure_1.default)();
+    const { ref, size } = (0, useMeasure_1.default)();
     const startWaveColor = '#0113b2';
     const stopWaveColor = '#050e54';
     const shadowX = 0;
     const shadowY = 5;
     const shadowSD = 15;
     const shadowOpacity = 0.5;
-    const wavesData = (0, stacked_1.calculateStacked)(1, width, height, 0.5);
+    const wavesData = (0, stacked_1.calculateStacked)(1, size.width, size.height, 0.5);
     return (react_1.default.createElement("div", { ref: ref, style: { width: '100%', height: '100%' } },
-        react_1.default.createElement("svg", { viewBox: `0 0 ${width} ${height}`, height: "100%", width: "100%" },
-            react_1.default.createElement("rect", { x: "0", y: "0", width: "540", height: "960", fill: "lightblue" }),
+        react_1.default.createElement("svg", { viewBox: `0 0 ${size.width} ${size.height}`, height: "100%", width: "100%" },
+            react_1.default.createElement("rect", { x: "0", y: "0", width: size.width, height: size.height, fill: "lightgreen" }),
             react_1.default.createElement("linearGradient", { id: "linear-gradient" },
                 react_1.default.createElement("stop", { offset: "0%", stopColor: startWaveColor, stopOpacity: "100%" }),
                 react_1.default.createElement("stop", { offset: "100%", stopColor: stopWaveColor, stopOpacity: "100%" })),
--- dist/esm/components/Wave.js
+++ dist/esm/components/Wave.js
@@ -2,17 +2,17 @@ import React from 'react';
 import useMeasure from '../utils/calculations/useMeasure';
 import { calculateStacked } from '../utils/calculations/stacked';
 const Wave = (props) => {
-    const [ref, { width, height }] = useMeasure();
+    const { ref, size } = useMeasure();
     const startWaveColor = '#0113b2';
     const stopWaveColor = '#050e54';
     const shadowX = 0;
     const shadowY = 5;
     const shadowSD = 15;
     const shadowOpacity = 0.5;
-    const wavesData = calculateStacked(1, width, height, 0.5);
+    const wavesData = calculateStacked(1, size.width, size.height, 0.5);
     return (React.createElement("div", { ref: ref, style: { width: '100%', height: '100%' } },
-        React.createElement("svg", { viewBox: `0 0 ${width} ${height}`, height: "100%", width: "100%" },
-            React.createElement("rect", { x: "0", y: "0", width: "540", height: "960", fill: "lightblue" }),
+        React.createElement("svg", { viewBox: `0 0 ${size.width} ${size.height}`, height: "100%", width: "100%" },
+            React.createElement("rect", { x: "0", y: "0", width: size.width, height: size.height, fill: "lightgreen" }),
             React.createElement("linearGradient", { id: "linear-gradient" },
                 React.createElement("stop", { offset: "0%", stopColor: startWaveColor, stopOpacity: "100%" }),
                 React.createElement("stop", { offset: "100%", stopColor: stopWaveColor, stopOpacity: "100%" })),
